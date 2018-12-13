f0a1a96e817d0e5cbd6db001c3244b02bfe83ec7
aksharchhowala <akshar.chhowala@gmail.com>
2018-12-13 15:15:30 +0530

    message

---
+++ app/src/main/java/com/example/akshar/nats/FirstAndroidApp.java
@@ -0,0 +1,11 @@
+package com.example.akshar.nats;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstAndroidApp extends Activity {
+public void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main_layout);
+}
+}
